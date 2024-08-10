229555d0c036702a3bc631fe76a6ca5b672f7e7a
IamHitori <demiangba33@gmail.com>
2024-08-10 17:00:12 -0600

    Carpeta de while donde se creo una secuencia del 1 al 10 con la funcion while

---
+++ while/secuencia.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+
+    int numero;
+
+    numero = 1;
+
+    while (numero<=10)
+    {
+        cout<<numero<<endl;
+        numero++;
+    }
+    
+
+    system("pause");
+    return 0;
+}
